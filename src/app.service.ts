7d0a36e7e3e768aac59bcd810d1db9ed5a15e3b8
frucvill <fructuoso.villarroel@gmail.com>
2024-05-10 20:04:11 -0400

    Config port in .env

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Inject } from '@nestjs/common';
+import configuration from './config/configuration';
+import { ConfigType } from '@nestjs/config';
 @Injectable()
 export class AppService {
+  constructor(@Inject(configuration.KEY) private configService: ConfigType<typeof configuration>){}
+
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Aqui el puerto determinado por el archivo .env  '+ this.configService.Port
   }
 }
